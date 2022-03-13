37bb2acf8717af35b4a43f036bc7ffc0562b84a9
Krish-bhardwaj <krishbhardwaj27203@gmail.com>
2022-03-13 13:22:42 +0530

    OK

---
+++ frontend/src/Components/Footer/Footer.js
@@ -0,0 +1,22 @@
+import "./Footer.scss";
+
+const Footer = () => {
+    return ( <
+        footer className = "footer" >
+        <
+        ul >
+        <
+        li > About us < /li> <
+        li > Contact < /li> <
+        li > For Query < /li> <
+        li > Career < /li> < /
+        ul > <
+        div >
+        <
+        p > & copy by Krish Bhardwaj < /p> < /
+        div > <
+        /footer>
+    );
+};
+
+export default Footer;
